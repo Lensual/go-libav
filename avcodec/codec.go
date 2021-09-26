e606993fac29029c1e9cb864bf7692092a406ccb
神楽坂喵喵 <lenvisual@gmail.com>
2021-09-27 01:17:40 +0800

    update codec

--- avcodec/codec.go
+++ avcodec/codec.go
@@ -19,6 +19,66 @@ func (cavcodec *CAVCodec) ID() uint32 {
 	return cavcodec.id
 }
 
+const (
+	/**
+	 * Codec uses only intra compression.
+	 * Video and audio codecs only.
+	 */
+	AV_CODEC_PROP_INTRA_ONLY = C.AV_CODEC_PROP_INTRA_ONLY
+
+	/**
+	 * Codec supports lossy compression. Audio and video codecs only.
+	 * @note a codec may support both lossy and lossless
+	 * compression modes
+	 */
+	AV_CODEC_PROP_LOSSY = C.AV_CODEC_PROP_LOSSY
+
+	/**
+	 * Codec supports lossless compression. Audio and video codecs only.
+	 */
+	AV_CODEC_PROP_LOSSLESS = C.AV_CODEC_PROP_LOSSLESS
+
+	/**
+	 * Codec supports frame reordering. That is, the coded order (the order in which
+	 * the encoded packets are output by the encoders / stored / input to the
+	 * decoders) may be different from the presentation order of the corresponding
+	 * frames.
+	 *
+	 * For codecs that do not have this property set, PTS and DTS should always be
+	 * equal.
+	 */
+	AV_CODEC_PROP_REORDER = C.AV_CODEC_PROP_REORDER
+
+	/**
+	 * Subtitle codec is bitmap based
+	 * Decoded AVSubtitle data can be read from the AVSubtitleRect->pict field.
+	 */
+	AV_CODEC_PROP_BITMAP_SUB = C.AV_CODEC_PROP_BITMAP_SUB
+
+	/**
+	 * Subtitle codec is text based.
+	 * Decoded AVSubtitle data can be read from the AVSubtitleRect->ass field.
+	 */
+	AV_CODEC_PROP_TEXT_SUB = C.AV_CODEC_PROP_TEXT_SUB
+
+	/**
+	 * @ingroup lavc_decoding
+	 * Required number of additionally allocated bytes at the end of the input bitstream for decoding.
+	 * This is mainly needed because some optimized bitstream readers read
+	 * 32 or 64 bit at once and could read over the end.<br>
+	 * Note: If the first 23 bits of the additional bytes are not 0, then damaged
+	 * MPEG bitstreams could cause overread and segfault.
+	 */
+	AV_INPUT_BUFFER_PADDING_SIZE = C.AV_INPUT_BUFFER_PADDING_SIZE
+
+	/**
+	 * @ingroup lavc_encoding
+	 * minimum encoding buffer size
+	 * Used to avoid some checks during header writing.
+	 */
+	AV_INPUT_BUFFER_MIN_SIZE = C.AV_INPUT_BUFFER_MIN_SIZE
+)
+
 /*
 Iterate over all registered codecs.
 
