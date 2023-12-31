a9ea70edd2224267633610652e3c58fc47dd430a
神楽坂喵喵 <lenvisual@gmail.com>
2023-12-31 17:21:52 +0000

    complete avcodec/codec.go

--- avcodec/codec.go
+++ avcodec/codec.go
@@ -3,7 +3,7 @@ package avcodec
 /*
 #cgo pkg-config: libavcodec
 
-#include "libavcodec/avcodec.h"
+#include "libavcodec/codec.h"
 */
 import "C"
 import (
@@ -12,6 +12,41 @@ import (
 	"github.com/Lensual/go-libav/avutil"
 	"github.com/Lensual/go-libav/ctypes"
 )
+
+/*
+ * AVCodec public API
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+//  #ifndef AVCODEC_CODEC_H
+//  #define AVCODEC_CODEC_H
+
+//  #include <stdint.h>
+
+//  #include "libavutil/avutil.h"
+//  #include "libavutil/hwcontext.h"
+//  #include "libavutil/log.h"
+//  #include "libavutil/pixfmt.h"
+//  #include "libavutil/rational.h"
+//  #include "libavutil/samplefmt.h"
+
+//  #include "libavcodec/codec_id.h"
+//  #include "libavcodec/version_major.h"
 
 /**
  * @addtogroup lavc_core
@@ -160,9 +195,14 @@ const (
  */
 type CAVProfile C.AVProfile
 
-// #region CAVCodec
+// #region CAVProfile
+
 func (p *CAVProfile) GetProfile() int {
 	return int(p.profile)
+}
+
+func (p *CAVProfile) SetProfile(profile int) {
+	p.profile = C.int(profile)
 }
 
 // /< short name for the profile
@@ -170,7 +210,16 @@ func (p *CAVProfile) GetName() string {
 	return C.GoString(p.name)
 }
 
-//#endregion CAVCodec
+// /< short name for the profile
+func (p *CAVProfile) SetName(name string) {
+	var cName *C.char = nil
+	if len(name) > 0 {
+		cName = C.CString(name)
+	}
+	p.name = cName
+}
+
+//#endregion CAVProfile
 
 /**
  * AVCodec.
@@ -190,6 +239,20 @@ func (c *CAVCodec) GetName() string {
 }
 
 /**
+ * Name of the codec implementation.
+ * The name is globally unique among encoders and among decoders (but an
+ * encoder and a decoder can share the same name).
+ * This is the primary way to find a codec from the user perspective.
+ */
+func (c *CAVCodec) SetName(name string) {
+	var cName *C.char = nil
+	if len(name) > 0 {
+		cName = C.CString(name)
+	}
+	c.name = cName
+}
+
+/**
  * Descriptive name for the codec, meant to be more human readable than name.
  * You should use the NULL_IF_CONFIG_SMALL() macro to define it.
  */
@@ -197,12 +260,30 @@ func (c *CAVCodec) GetLongName() string {
 	return C.GoString(c.long_name)
 }
 
+/**
+ * Descriptive name for the codec, meant to be more human readable than name.
+ * You should use the NULL_IF_CONFIG_SMALL() macro to define it.
+ */
+func (c *CAVCodec) SetLongName(longName string) {
+	var cLongName *C.char = nil
+	if len(longName) > 0 {
+		cLongName = C.CString(longName)
+	}
+	c.long_name = cLongName
+}
+
 func (c *CAVCodec) GetType() avutil.CAVMediaType {
 	return avutil.CAVMediaType(c._type)
+}
+func (c *CAVCodec) SetType(_type avutil.CAVMediaType) {
+	c._type = C.enum_AVMediaType(_type)
 }
 
 func (c *CAVCodec) GetId() CAVCodecID {
 	return CAVCodecID(c.id)
+}
+func (c *CAVCodec) SetId(id CAVCodecID) {
+	c.id = C.enum_AVCodecID(id)
 }
 
 /**
@@ -213,9 +294,22 @@ func (c *CAVCodec) GetCapabilities() int {
 	return int(c.capabilities)
 }
 
+/**
+ * Codec capabilities.
+ * see AV_CODEC_CAP_*
+ */
+func (c *CAVCodec) SetCapabilities(capabilities int) {
+	c.capabilities = C.int(capabilities)
+}
+
 // /< maximum value for lowres supported by the decoder
 func (c *CAVCodec) GetMaxLowres() uint8 {
 	return uint8(c.max_lowres)
+}
+
+// /< maximum value for lowres supported by the decoder
+func (c *CAVCodec) SetMaxLowres(maxLowres uint8) {
+	c.max_lowres = C.uint8_t(maxLowres)
 }
 
 // /< array of supported framerates, or NULL if any, array is terminated by {0,0}
@@ -223,9 +317,19 @@ func (c *CAVCodec) GetSupportedFramerates() *avutil.CAVRational {
 	return (*avutil.CAVRational)(unsafe.Pointer(c.supported_framerates))
 }
 
+// /< array of supported framerates, or NULL if any, array is terminated by {0,0}
+func (c *CAVCodec) SetSupportedFramerates(supportedFramerates *avutil.CAVRational) {
+	c.supported_framerates = (*C.AVRational)(unsafe.Pointer(supportedFramerates))
+}
+
 // /< array of supported pixel formats, or NULL if unknown, array is terminated by -1
 func (c *CAVCodec) GetPixFmts() *avutil.CAVPixelFormat {
 	return (*avutil.CAVPixelFormat)(c.pix_fmts)
+}
+
+// /< array of supported pixel formats, or NULL if unknown, array is terminated by -1
+func (c *CAVCodec) SetPixFmts(pixFmts *avutil.CAVPixelFormat) {
+	c.pix_fmts = (*C.enum_AVPixelFormat)(pixFmts)
 }
 
 // /< array of supported audio samplerates, or NULL if unknown, array is terminated by 0
@@ -233,9 +337,19 @@ func (c *CAVCodec) GetSupportedSamplerates() *ctypes.Int {
 	return (*ctypes.Int)(c.supported_samplerates)
 }
 
+// /< array of supported audio samplerates, or NULL if unknown, array is terminated by 0
+func (c *CAVCodec) SetSupportedSamplerates(supportedSamplerates *ctypes.Int) {
+	c.supported_samplerates = (*C.int)(supportedSamplerates)
+}
+
 // /< array of supported sample formats, or NULL if unknown, array is terminated by -1
 func (c *CAVCodec) GetSampleFmts() *avutil.CAVSampleFormat {
 	return (*avutil.CAVSampleFormat)(c.sample_fmts)
+}
+
+// /< array of supported sample formats, or NULL if unknown, array is terminated by -1
+func (c *CAVCodec) SetSampleFmts(sampleFmts *avutil.CAVSampleFormat) {
+	c.sample_fmts = (*C.enum_AVSampleFormat)(sampleFmts)
 }
 
 //  #if FF_API_OLD_CHANNEL_LAYOUT
@@ -251,9 +365,19 @@ func (c *CAVCodec) GetPrivClass() *avutil.CAVClass {
 	return (*avutil.CAVClass)(unsafe.Pointer(c.priv_class))
 }
 
+// /< AVClass for the private context
+func (c *CAVCodec) SetPrivClass(privClass *avutil.CAVClass) {
+	c.priv_class = (*C.AVClass)(unsafe.Pointer(privClass))
+}
+
 // /< array of recognized profiles, or NULL if unknown, array is terminated by {AV_PROFILE_UNKNOWN}
-func (c *CAVCodec) GetProfiles() *C.AVProfile {
-	return c.profiles
+func (c *CAVCodec) GetProfiles() *CAVProfile {
+	return (*CAVProfile)(c.profiles)
+}
+
+// /< array of recognized profiles, or NULL if unknown, array is terminated by {AV_PROFILE_UNKNOWN}
+func (c *CAVCodec) SetProfiles(profiles *CAVProfile) {
+	c.profiles = (*C.AVProfile)(profiles)
 }
 
 /**
@@ -271,106 +395,68 @@ func (c *CAVCodec) GetWrapperName() string {
 }
 
 /**
+ * Group name of the codec implementation.
+ * This is a short symbolic name of the wrapper backing this codec. A
+ * wrapper uses some kind of external implementation for the codec, such
+ * as an external library, or a codec implementation provided by the OS or
+ * the hardware.
+ * If this field is NULL, this is a builtin, libavcodec native codec.
+ * If non-NULL, this will be the suffix in AVCodec.name in most cases
+ * (usually AVCodec.name will be of the form "<codec_name>_<wrapper_name>").
+ */
+func (c *CAVCodec) SetWrapperName(wrapperName string) {
+	var cWrapperName *C.char = nil
+	if len(wrapperName) > 0 {
+		cWrapperName = C.CString(wrapperName)
+	}
+	c.wrapper_name = cWrapperName
+}
+
+/**
  * Array of supported channel layouts, terminated with a zeroed layout.
  */
 func (c *CAVCodec) GetChLayouts() *avutil.CAVChannelLayout {
 	return (*avutil.CAVChannelLayout)(unsafe.Pointer(c.ch_layouts))
 }
 
+/**
+ * Array of supported channel layouts, terminated with a zeroed layout.
+ */
+func (c *CAVCodec) SetChLayouts(chLayouts *avutil.CAVChannelLayout) {
+	c.ch_layouts = (*C.AVChannelLayout)(unsafe.Pointer(chLayouts))
+}
+
 //#endregion CAVCodec
 
-//TODO recheck header
-// const (
-// 	/**
-// 	 * Codec uses only intra compression.
-// 	 * Video and audio codecs only.
-// 	 */
-// 	AV_CODEC_PROP_INTRA_ONLY = C.AV_CODEC_PROP_INTRA_ONLY
-
-// 	/**
-// 	 * Codec supports lossy compression. Audio and video codecs only.
-// 	 * @note a codec may support both lossy and lossless
-// 	 * compression modes
-// 	 */
-// 	AV_CODEC_PROP_LOSSY = C.AV_CODEC_PROP_LOSSY
-
-// 	/**
-// 	 * Codec supports lossless compression. Audio and video codecs only.
-// 	 */
-// 	AV_CODEC_PROP_LOSSLESS = C.AV_CODEC_PROP_LOSSLESS
-
-// 	/**
-// 	 * Codec supports frame reordering. That is, the coded order (the order in which
-// 	 * the encoded packets are output by the encoders / stored / input to the
-// 	 * decoders) may be different from the presentation order of the corresponding
-// 	 * frames.
-// 	 *
-// 	 * For codecs that do not have this property set, PTS and DTS should always be
-// 	 * equal.
-// 	 */
-// 	AV_CODEC_PROP_REORDER = C.AV_CODEC_PROP_REORDER
-
-// 	/**
-// 	 * Subtitle codec is bitmap based
-// 	 * Decoded AVSubtitle data can be read from the AVSubtitleRect->pict field.
-// 	 */
-// 	AV_CODEC_PROP_BITMAP_SUB = C.AV_CODEC_PROP_BITMAP_SUB
-
-// 	/**
-// 	 * Subtitle codec is text based.
-// 	 * Decoded AVSubtitle data can be read from the AVSubtitleRect->ass field.
-// 	 */
-// 	AV_CODEC_PROP_TEXT_SUB = C.AV_CODEC_PROP_TEXT_SUB
-
-// 	/**
-// 	 * @ingroup lavc_decoding
-// 	 * Required number of additionally allocated bytes at the end of the input bitstream for decoding.
-// 	 * This is mainly needed because some optimized bitstream readers read
-// 	 * 32 or 64 bit at once and could read over the end.<br>
-// 	 * Note: If the first 23 bits of the additional bytes are not 0, then damaged
-// 	 * MPEG bitstreams could cause overread and segfault.
-// 	 */
-// 	AV_INPUT_BUFFER_PADDING_SIZE = C.AV_INPUT_BUFFER_PADDING_SIZE
-
-// 	/**
-// 	 * @ingroup lavc_encoding
-// 	 * minimum encoding buffer size
-// 	 * Used to avoid some checks during header writing.
-// 	 */
-// 	AV_INPUT_BUFFER_MIN_SIZE = C.AV_INPUT_BUFFER_MIN_SIZE
-// )
-
-/*
-Iterate over all registered codecs.
-
-@param opaque a pointer where libavcodec will store the iteration state. Must
-
-	point to NULL to start the iteration.
-
-@return the next registered codec or NULL when the iteration is
-
-	finished
-*/
+/**
+ * Iterate over all registered codecs.
+ *
+ * @param opaque a pointer where libavcodec will store the iteration state. Must
+ *               point to NULL to start the iteration.
+ *
+ * @return the next registered codec or NULL when the iteration is
+ *         finished
+ */
 func AvCodecIterate(opaque *unsafe.Pointer) *CAVCodec {
 	return (*CAVCodec)(C.av_codec_iterate(opaque))
 }
 
-/*
-Find a registered decoder with a matching codec ID.
-
-	@param id AVCodecID of the requested decoder
-	@return A decoder if one was found, NULL otherwise.
-*/
+/**
+ * Find a registered decoder with a matching codec ID.
+ *
+ * @param id AVCodecID of the requested decoder
+ * @return A decoder if one was found, NULL otherwise.
+ */
 func AvcodecFindDecoder(id CAVCodecID) *CAVCodec {
 	return (*CAVCodec)(C.avcodec_find_decoder(uint32(id)))
 }
 
-/*
-Find a registered decoder with the specified name.
-
-	@param name name of the requested decoder
-	@return A decoder if one was found, NULL otherwise.
-*/
+/**
+ * Find a registered decoder with the specified name.
+ *
+ * @param name name of the requested decoder
+ * @return A decoder if one was found, NULL otherwise.
+ */
 func AvcodecFindDecoderByName(name string) *CAVCodec {
 	var cName *C.char = nil
 	if len(name) > 0 {
@@ -381,22 +467,22 @@ func AvcodecFindDecoderByName(name string) *CAVCodec {
 	return (*CAVCodec)(C.avcodec_find_decoder_by_name(cName))
 }
 
-/*
-Find a registered encoder with a matching codec ID.
-
-	@param id AVCodecID of the requested encoder
-	@return An encoder if one was found, NULL otherwise.
-*/
+/**
+ * Find a registered encoder with a matching codec ID.
+ *
+ * @param id AVCodecID of the requested encoder
+ * @return An encoder if one was found, NULL otherwise.
+ */
 func AvcodecFindEncoder(id CAVCodecID) *CAVCodec {
 	return (*CAVCodec)(C.avcodec_find_encoder(uint32(id)))
 }
 
-/*
-Find a registered encoder with the specified name.
-
-	@param name name of the requested encoder
-	@return An encoder if one was found, NULL otherwise.
-*/
+/**
+ * Find a registered encoder with the specified name.
+ *
+ * @param name name of the requested encoder
+ * @return An encoder if one was found, NULL otherwise.
+ */
 func AvcodecFindEncoderByName(name string) *CAVCodec {
 	var cName *C.char = nil
 	if len(name) > 0 {
@@ -406,3 +492,151 @@ func AvcodecFindEncoderByName(name string) *CAVCodec {
 
 	return (*CAVCodec)(C.avcodec_find_encoder_by_name(cName))
 }
+
+/**
+ * @return a non-zero number if codec is an encoder, zero otherwise
+ */
+func AvCodecIsEncoder(codec *CAVCodec) int {
+	return int(C.av_codec_is_encoder((*C.AVCodec)(codec)))
+}
+
+/**
+ * @return a non-zero number if codec is a decoder, zero otherwise
+ */
+func AvCodecIsDecoder(codec *CAVCodec) int {
+	return int(C.av_codec_is_decoder((*C.AVCodec)(codec)))
+}
+
+/**
+ * Return a name for the specified profile, if available.
+ *
+ * @param codec the codec that is searched for the given profile
+ * @param profile the profile value for which a name is requested
+ * @return A name for the profile if found, NULL otherwise.
+ */
+func AvGetProfileName(codec *CAVCodec, profile int) string {
+	return C.GoString(C.av_get_profile_name((*C.AVCodec)(codec), C.int(profile)))
+}
+
+const (
+	/**
+	 * The codec supports this format via the hw_device_ctx interface.
+	 *
+	 * When selecting this format, AVCodecContext.hw_device_ctx should
+	 * have been set to a device of the specified type before calling
+	 * avcodec_open2().
+	 */
+	AV_CODEC_HW_CONFIG_METHOD_HW_DEVICE_CTX = C.AV_CODEC_HW_CONFIG_METHOD_HW_DEVICE_CTX
+	/**
+	 * The codec supports this format via the hw_frames_ctx interface.
+	 *
+	 * When selecting this format for a decoder,
+	 * AVCodecContext.hw_frames_ctx should be set to a suitable frames
+	 * context inside the get_format() callback.  The frames context
+	 * must have been created on a device of the specified type.
+	 *
+	 * When selecting this format for an encoder,
+	 * AVCodecContext.hw_frames_ctx should be set to the context which
+	 * will be used for the input frames before calling avcodec_open2().
+	 */
+	AV_CODEC_HW_CONFIG_METHOD_HW_FRAMES_CTX = C.AV_CODEC_HW_CONFIG_METHOD_HW_FRAMES_CTX
+	/**
+	 * The codec supports this format by some internal method.
+	 *
+	 * This format can be selected without any additional configuration -
+	 * no device or frames context is required.
+	 */
+	AV_CODEC_HW_CONFIG_METHOD_INTERNAL = C.AV_CODEC_HW_CONFIG_METHOD_INTERNAL
+	/**
+	 * The codec supports this format by some ad-hoc method.
+	 *
+	 * Additional settings and/or function calls are required.  See the
+	 * codec-specific documentation for details.  (Methods requiring
+	 * this sort of configuration are deprecated and others should be
+	 * used in preference.)
+	 */
+	AV_CODEC_HW_CONFIG_METHOD_AD_HOC = C.AV_CODEC_HW_CONFIG_METHOD_AD_HOC
+)
+
+type CAVCodecHWConfig C.AVCodecHWConfig
+
+//#region CAVCodecHWConfig
+
+/**
+ * For decoders, a hardware pixel format which that decoder may be
+ * able to decode to if suitable hardware is available.
+ *
+ * For encoders, a pixel format which the encoder may be able to
+ * accept.  If set to AV_PIX_FMT_NONE, this applies to all pixel
+ * formats supported by the codec.
+ */
+func (hwconfig CAVCodecHWConfig) GetPixFmt() avutil.CAVPixelFormat {
+	return avutil.CAVPixelFormat(hwconfig.pix_fmt)
+}
+
+/**
+ * For decoders, a hardware pixel format which that decoder may be
+ * able to decode to if suitable hardware is available.
+ *
+ * For encoders, a pixel format which the encoder may be able to
+ * accept.  If set to AV_PIX_FMT_NONE, this applies to all pixel
+ * formats supported by the codec.
+ */
+func (hwconfig *CAVCodecHWConfig) SetPixFmt(pixFmt avutil.CAVPixelFormat) {
+	hwconfig.pix_fmt = C.enum_AVPixelFormat(pixFmt)
+}
+
+/**
+ * Bit set of AV_CODEC_HW_CONFIG_METHOD_* flags, describing the possible
+ * setup methods which can be used with this configuration.
+ */
+func (hwconfig CAVCodecHWConfig) GetMethods() int {
+	return int(hwconfig.methods)
+}
+
+/**
+ * Bit set of AV_CODEC_HW_CONFIG_METHOD_* flags, describing the possible
+ * setup methods which can be used with this configuration.
+ */
+func (hwconfig *CAVCodecHWConfig) SetMethods(methods int) {
+	hwconfig.methods = C.int(methods)
+}
+
+/**
+ * The device type associated with the configuration.
+ *
+ * Must be set for AV_CODEC_HW_CONFIG_METHOD_HW_DEVICE_CTX and
+ * AV_CODEC_HW_CONFIG_METHOD_HW_FRAMES_CTX, otherwise unused.
+ */
+func (hwconfig CAVCodecHWConfig) GetDeviceType() C.enum_AVHWDeviceType {
+	return hwconfig.device_type
+}
+
+/**
+ * The device type associated with the configuration.
+ *
+ * Must be set for AV_CODEC_HW_CONFIG_METHOD_HW_DEVICE_CTX and
+ * AV_CODEC_HW_CONFIG_METHOD_HW_FRAMES_CTX, otherwise unused.
+ */
+func (hwconfig *CAVCodecHWConfig) SetDeviceType(deviceType C.enum_AVHWDeviceType) {
+	hwconfig.device_type = deviceType
+}
+
+//#endregion CAVCodecHWConfig
+
+/**
+ * Retrieve supported hardware configurations for a codec.
+ *
+ * Values of index from zero to some maximum return the indexed configuration
+ * descriptor; all other values return NULL.  If the codec does not support
+ * any hardware configurations then it will always return NULL.
+ */
+func AvcodecGetHwConfig(codec *CAVCodec, index int) *CAVCodecHWConfig {
+	return (*CAVCodecHWConfig)(C.avcodec_get_hw_config((*C.AVCodec)(codec), C.int(index)))
+}
+
+/**
+ * @}
+ */
+
+//  #endif /* AVCODEC_CODEC_H */
